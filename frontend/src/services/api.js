01fe3177ee6b1716a10a29142bf5b6f418688be8
farid <abbaslifarid2006@gmail.com>
2026-05-18 03:08:59 +0400

    fix api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5072/api';
+const API_BASE_URL = 'http://13.51.158.60:5072/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
